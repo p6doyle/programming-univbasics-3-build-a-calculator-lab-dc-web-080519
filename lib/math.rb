d327fa37056730c9b113be87176af2e8c8df07ad
Peyton Doyle <p6doyle@gmail.com>
2019-07-15 20:47:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -12,15 +12,21 @@ num1 - num2
 end
 
 def division(num1, num2)
-
+num1 == 5
+num2 == 4
+num1 / num2
 end
 
 def multiplication(num1, num2)
-
+num1 == 5
+num2 == 4
+num1 * num2
 end
 
 def modulo(num1, num2)
-
+num1 == 5
+num2 == 4
+num1 / num2
 end
 
 def square_root(num)
